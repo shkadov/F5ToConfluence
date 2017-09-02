65181fa2005f6f25deb684cb324d9941caa4f425
Den Shkadov <den.shkadov@namecheap.com>
2017-09-02 16:25:29 +0300

    get json + get attachments id + remove attachments

--- f5ToConfluence.py
+++ f5ToConfluence.py
@@ -1,24 +1,49 @@
 import json
 import requests
+import jira
 
+
+user = 'user'
+pwd =  "password"
 #Create JSON file
-def FileCreation():
-    r = requests.get('https://confluence/rest/api/content/pageId/child/attachment', auth=('', ''), stream=True)
+def file_creation():
+    r = requests.get('https://confluence/rest/api/content/60606188/child/attachment', auth=(user, pwd), stream=True)
     data = r.json()
     with open('data.json', 'w') as file:
         json.dump(data, file)
 
 #Get attachment's IDs from JSON file
-def getAttachmentsID():
-    FileCreation()
+def get_attachmentsid():
+    file_creation()
     with open('data.json')as data_file:
         data = json.load(data_file)
-        attachmentID = [item['id'] for item in data[u'results']]
+        attachmentid = [item['id'] for item in data[u'results']]
     file = open('attachmentID.txt', 'w')
-    for i in attachmentID:
+    for i in attachmentid:
         file.write(i + '\n')
     file.close()
 
+def remove_attachments():
+    file = open('attachmentID.txt', 'r')
+    for i in file:
+        headers = {'Content-Type': 'application/json'}
+        # response = requests.delete(url, auth=(user, pwd))
+        user = 'user'
+        pwd = "password"
+        url = "https://confluence/rest/api/content/" + str(i)
+       # print(url)
+        response = requests.get(url, auth=(user, pwd))
+        print(str(url) + '=>' + str(response.status_code))
 
+#def upload_new_attachments():
 
-getAttachmentsID()
+#get_attachmentsid()
+def remove_attachments():
+
+    file = open('attachmentID.txt', 'r')
+    url = "https://confluence/rest/api/content/"
+    for i in file:
+        headers = {'Content-Type': 'application/json'}
+        link = url + str(i).strip()
+        response = requests.delete(str(link), auth=(user, pwd), headers=headers)
+        print(link +" " + str(response.status_code))
