a3332d950ebb316a2c5be9eb26838cf1cf1bbe7f
Den Shkadov <den.shkadov@namecheap.com>
2017-09-03 10:52:04 +0300

    refactored

--- f5ToConfluence.py
+++ f5ToConfluence.py
@@ -13,15 +13,16 @@ import os.path
 
 user = 'user'
 pwd = 'password'
-url = 'https://confluence/rest/api/content/60606188/child/attachment'
+url = 'https://confluence/rest/api/content/'
+pageid = 'pageID'
+
 #Create JSON file
 def file_creation():
-    r = requests.get(url, auth=(user, pwd), stream=True)
+    r = requests.get(url + pageid + '/child/attachment', auth=(user, pwd), stream=True)
     data = r.json()
     with open('data.json', 'w') as file:
         json.dump(data, file)
-
-#file_creation()
+    print('JSON file has been successfully created')
 
 #Get attachment's IDs from JSON file
 def get_attachmentsid():
@@ -33,47 +34,30 @@ def get_attachmentsid():
     for i in attachmentid:
         file.write(i + '\n')
     file.close()
+    print('Attachments file has been successfully created')
 
-def remove_attachments():
-    file = open('attachmentID.txt', 'r')
-    for i in file:
-        headers = {'Content-Type': 'application/json'}
-        # response = requests.delete(url, auth=(user, pwd))
-        #user = 'user'
-        #pwd = "password"
-        url = "https://confluence/rest/api/content/" + str(i)
-       # print(url)
-        response = requests.get(url, auth=(user, pwd))
-        print(str(url) + '=>' + str(response.status_code))
-
-#def upload_new_attachments():
-
-#get_attachmentsid()
 def remove_attachments():
 
     file = open('attachmentID.txt', 'r')
-    url = "https://confluence/rest/api/content/"
     for i in file:
         headers = {'Content-Type': 'application/json'}
         link = url + str(i).strip()
         response = requests.delete(str(link), auth=(user, pwd), headers=headers)
         print(link +" " + str(response.status_code))
+    print('Previous attachments have been successfully removed from ' + url + pageid)
 
 def upload_attachments():
     imagedir = 'c:\img\Common'
     list = os.listdir(imagedir)
     for file in list:
-        url = 'https://confluence/rest/api/content/60606188/child/attachment'
         headers = {'X-Atlassian-Token': 'no-check'}
         content_type = 'image/jpeg'
-        #auth = (user, pwd)
         files = {'file': (file, open(file, 'rb'), 'image/jpeg')}
         print(files)
-        r = requests.post(url, headers=headers, files=files, auth=auth)
+        r = requests.post(url + pageid + '/child/attachment', headers=headers, files=files, auth=(user, pwd))
+    print('New attachments have been successfully uploaded to ' + url + pageid)
 
-
-#upload_attachments()
-
-
-
-
+file_creation()
+get_attachmentsid()
+remove_attachments()
+upload_attachments()
